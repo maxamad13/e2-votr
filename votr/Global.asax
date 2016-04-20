40ba0c1c0caa07d3a5aef5964c967f45baca6189
Mohamed Sharif <mohame@gmail.com>
2016-04-20 18:31:13 -0500

    first commit

---
+++ votr/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="votr.MvcApplication" Language="C#" %>
---
+++ votr/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(votr.Startup))]
+namespace votr
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
