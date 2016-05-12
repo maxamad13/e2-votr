b6023c6356321b91a49f3313a2316db11d4ed625
Jurnell Cockhren <jurnell@sophicware.com>
2016-05-11 17:35:00 -0700

    IMplemented form for capturing Poll and option data.

--- Votr/Scripts/app.js
+++ Votr/Scripts/app.js
@@ -7,4 +7,16 @@ app.controller("PollCtrl", function () {
     self.showPopup = function () {
         window.alert();
     }
+    self.Options = [];
+
+    self.AddOption = function (e) {
+        // Get value from input field
+        var input_val = $("#firstoption").val();
+        var incrementer = 1;
+        self.Options.push(input_val);
+        $("#firstoption").val("");
+        // Then append it onto the Options object
+        e.preventDefault();
+
+    }
 });
